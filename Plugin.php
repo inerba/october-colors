030da08f9f7add4825b96f96741d196d904795bc
Inerba <inerba@gmail.com>
2017-07-27 09:31:17 +0000

    init

---
+++ Plugin.php
@@ -0,0 +1,51 @@
+<?php namespace Inerba\Colors;
+
+use Backend;
+use System\Classes\PluginBase;
+use Inerba\Colors\Classes\ColorBase;
+
+/**
+ * Colors Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Colors',
+            'description' => 'No description provided yet...',
+            'author'      => 'Inerba',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    public function registerMarkupTags()
+    {
+        return [
+            'functions' => [
+                'color_lighten'       => [ColorBase::class, 'lighten'],
+                'color_darken'        => [ColorBase::class, 'darken'],
+                'color_isLight'       => [ColorBase::class, 'isLight'],
+                'color_isDark'        => [ColorBase::class, 'isDark'],
+                'color_complementary' => [ColorBase::class, 'complementary'],
+                'color_greyscale'     => [ColorBase::class, 'greyscale'],
+                'color_desaturate'    => [ColorBase::class, 'desaturate'],
+                'color_rgbToHex'      => [ColorBase::class, 'rgbToHex'],
+                'color_hexToRgb'      => [ColorBase::class, 'hexToRgb'],
+                'color_gradient'      => [ColorBase::class, 'getCssGradient'],
+                'color_mix'           => [ColorBase::class, 'mix'],
+                
+                'color_random'        => [ColorBase::class, 'random'],
+
+                'color_extract'       => [ColorBase::class, 'extract'],
+                'color_imgLight'      => [ColorBase::class, 'imgLight'],
+            ]
+        ];
+    }
+
+}
---
+++ classes/ColorBase.php
@@ -0,0 +1,185 @@
+<?php namespace Inerba\Colors\Classes;
+
+use Cache;
+use Mexitek\PHPColors\Color as PHPColors;
+use Colorizer\Colorize;
+use League\ColorExtractor\Color;
+use League\ColorExtractor\ColorExtractor;
+use League\ColorExtractor\Palette;
+
+class ColorBase {
+
+    public static function lighten($color, $percent=20)
+    {
+        $color = new PHPColors($color);
+        return '#' . $color->lighten($percent);
+    }
+
+    public static function darken($color, $percent=20)
+    {
+        $color = new PHPColors($color);
+        return '#' . $color->darken($percent);
+    }
+
+    public static function isLight($color)
+    {
+        $color = new PHPColors($color);
+        return $color->isLight();
+    }
+
+    public static function isDark($color)
+    {
+        $color = new PHPColors($color);
+        return $color->isDark();
+    }
+
+    public static function complementary($color)
+    {
+        $color = new PHPColors($color);
+        return '#' . $color->complementary($color);
+    }
+
+    public static function rgbToHex($r,$g,$b)
+    {
+        return '#' . PHPColors::rgbToHex(['R'=>$r,'G'=>$g,'B'=>$b]);
+    }
+
+    public static function greyscale($color)
+    {
+        $rgb = PHPColors::hexToRgb($color);
+
+        $ds = $rgb['R'] * 0.3 + $rgb['G'] * 0.3 + $rgb['B'] * 0.3;
+
+        return '#' . PHPColors::rgbToHex(['R'=>$ds,'G'=>$ds,'B'=>$ds]);
+
+    }
+
+    public static function desaturate($color,$saturation)
+    {
+        $hsl = PHPColors::hexToHsl($color);
+
+        $saturation = $saturation/100;
+
+        $hsl['S'] = $hsl['S'] - $saturation < 0 ? 0 : $hsl['S'] - $saturation;
+
+        //dd($hsl,$saturation);
+
+        return '#' . PHPColors::hslToHex($hsl);
+
+    }
+
+    public static function random($string=false,$normalize=false)
+    {
+        $colorize = new Colorize();
+
+        if(!$string) $string = self::randomString();
+
+        $color = $colorize->text($string);
+
+        if(is_array($normalize)) {
+            $color = $color->normalize($normalize[0], $normalize[1]);
+        }
+
+        return $color->hex();
+
+    }
+
+    public static function hexToRgb($hex,$alpha=false)
+    {
+        $rgb = PHPColors::hexToRgb($hex);
+
+        if($alpha) return "rgba({$rgb['R']},{$rgb['G']},{$rgb['B']},{$alpha});";
+        
+        return "rgb({$rgb['R']},{$rgb['G']},{$rgb['B']});";
+    }
+
+    public static function getCssGradient($color,$steps,$old_browsers = true)
+    {
+        $color = new PHPColors($color);
+        return $color->getCssGradient($steps,$old_browsers);
+    }
+
+    public static function mix($color,$color2, $amount = 0)
+    {
+        $color = new PHPColors($color);
+        return '#' . $color->mix($color2, $amount);
+    }
+
+    public static function extract($path,$num=5)
+    {
+        $key = 'colors-extract'.$path.$num;
+        $extract = Cache::rememberForever($key, function() use($path,$num) {
+            $palette = Palette::fromFilename(base_path($path));
+            //$most_used_colors = $palette->getMostUsedColors($num);
+            $extractor = new ColorExtractor($palette);
+            $most_used_colors = $extractor->extract($num);
+
+            foreach ($most_used_colors as $colorInt) {
+                $color[] = Color::fromIntToHex($colorInt);
+            }
+
+            return $color;
+        });
+
+        return $extract;
+    }
+
+    public static function imgLight($filename,$num_samples=10)
+    {
+        $key = 'colors-imgLight'.$filename.$num_samples;
+        
+        $luminance = Cache::rememberForever($key, function() use ($filename, $num_samples) {
+            return self::getluminance(base_path($filename), $num_samples);
+        });
+
+        return $luminance > 170 ? true : false;
+    }
+
+    /* outils */
+    private static function getluminance($filename, $num_samples=10)
+    {
+        $img = imagecreatefromjpeg($filename);
+
+        $width = imagesx($img);
+        $height = imagesy($img);
+
+        $x_step = intval($width/$num_samples);
+        $y_step = intval($height/$num_samples);
+
+        $total_lum = 0;
+
+        $sample_no = 1;
+
+        for ($x=0; $x<$width; $x+=$x_step) {
+            for ($y=0; $y<$height; $y+=$y_step) {
+
+                $rgb = imagecolorat($img, $x, $y);
+                $r = ($rgb >> 16) & 0xFF;
+                $g = ($rgb >> 8) & 0xFF;
+                $b = $rgb & 0xFF;
+
+                $lum = ($r+$r+$b+$g+$g+$g)/6;
+
+                $total_lum += $lum;
+
+                $sample_no++;
+            }
+        }
+
+        // work out the average
+        $avg_lum  = $total_lum/$sample_no;
+
+        return $avg_lum;
+    }
+
+    private static function randomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+}
